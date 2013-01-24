327900034c1acaba8c82ff5af0d9dff85b83611d
rjp <rjpartington@googlemail.com>
2013-01-24 22:59:20 +0000

    When we appear, print out the clipboard contents for debugging

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,4 +2,8 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     true
   end
+
+  def applicationDidBecomeActive(application)
+      puts "Clipboard: " << UIPasteboard::generalPasteboard.string
+  end
 end
